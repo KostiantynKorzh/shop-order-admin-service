aab6359d0e8a92c750433d61a9f83ab8fbe3815a
Kostiantyn Korzh <kostiantyn_korzh@epam.com>
2021-09-02 04:56:01 +0300

    Test commit

--- src/main/kotlin/study/me/db/DatabaseConfig.kt
+++ src/main/kotlin/study/me/db/DatabaseConfig.kt
@@ -8,7 +8,7 @@ class DatabaseConfig {
     fun initDb(): Database {
         val host = System.getenv("MYSQL_HOST")
         return Database.connect(
-            "jdbc:mysql://$host:3306/orders_db?allowPublicKeyRetrieval=true&useSSL=false",
+            "jdbc:mysql://$host:3306/orders_db?allowPublicKeyRetrieval=true&useSSL=false&?zeroDateTimeBehavior=convertToNull",
             user = "root", password = System.getenv("MYSQL_ROOT_PASSWORD")
         )
     }
